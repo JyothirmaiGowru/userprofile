58a61ddd27b15042f10b5ca33292e4590d1a8091
Jyothirmai <nxtwave@ip-10-2-44-143.ap-south-1.compute.internal>
2024-06-08 03:21:30 +0000

    commit messge

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import UserProfile from '.Components/UserProfile/index'
+
+const userdetails = {
+  imageurl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+  name: 'Esther Howard',
+  role: 'Software Developer',
+}
+
+const App = () => <UserProfile />
+export default App
