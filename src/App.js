415b16cc31a106cbcc29267eae823b5f2c27ee48
Jyothi <gowrujyothirmai@gmail.com>
2024-06-08 13:03:29 +0000

    user_profile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import UserProfile from './Components/UserProfile/index'
+import UserProfile from './Components/UserProfile'
 
-const userdetails = {
-  imageurl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
-  name: 'Esther Howard',
-  role: 'Software Developer',
-}
+import './App.css'
 
-const App = () => <UserProfile userdetails={userdetails} />
+const UserDetailsList = [
+  {
+    uniqueNo: 1,
+    imageurl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Esther Howard',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 2,
+    imageurl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Esther Howard',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 3,
+    imageurl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Esther Howard',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 4,
+    imageurl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Esther Howard',
+    role: 'Software Developer',
+  },
+]
+
+const App = () => (
+  <div className="list-container">
+    <h1 className="title">UserList</h1>
+    <ul>
+      {UserDetailsList.map(eachItem => (
+        <UserProfile userdetails={eachItem} key={eachItem.uniqueNo} />
+      ))}
+    </ul>
+  </div>
+)
 export default App
--- src/Components/UserProfile/index.js
+++ src/Components/UserProfile/index.js
@@ -1,9 +1,17 @@
+import './index.css'
+
 const UserProfile = props => {
   const {userdetails} = props
-  const {imageurl} = userdetails
+  const {imageurl, name, role} = userdetails
 
   return (
-    <div><img src={imageurl} /> </div>
+    <div className="user-card-container">
+      <img src={imageurl} className="avatar" alt="avatar" />
+      <div className="user-details-container">
+        <h1 className="user-name">{name}</h1>
+        <p className="user-designation">{role}</p>
+      </div>
+    </div>
   )
 }
 
